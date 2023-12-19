02e46091e9236c9946f4641e68c7d9d9afff48e1
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2023-12-19 18:11:11 -0500

    web-portfolio files added

---
+++ web-portfolio/portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import 'bulma/css/bulma.css';
+import './App.css'
+
+
+const App = () => {
+  return <header>
+    <h1>Sawyer Nicastro's Amazing Projects</h1>
+    <h2>Here are some of my projects</h2>
+    </header>
+  ;
+};
+
+export default App;
